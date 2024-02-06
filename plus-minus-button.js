dacc4f380b89bf8afba4db4f9651f5c3d45e0e50
Michael Geduldig <MGeduldi@win.cccis.com>
2024-02-06 11:45:56 -0600

    organizing js files

--- plus-minus-button.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('#checkbox1').change(function() {
-      if (this.checked) {
-        $(this).closest('.card').addClass('selected');
-      } else {
-        $(this).closest('.card').removeClass('selected');
-      }
-    });
-  });
