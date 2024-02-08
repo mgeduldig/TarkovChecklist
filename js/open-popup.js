b9b20f31df910261937ddd2a8dc7c8e760e5bd6e
mgeduldig <mgeduldig0@gmail.com>
2024-02-08 12:22:48 -0600

    added donation popup

---
+++ js/open-popup.js
@@ -0,0 +1,7 @@
+document.getElementById('openPopup').addEventListener('click',function() {
+	document.getElementById('popupContainer').style.display='flex';
+});
+
+function closePopup() {
+	document.getElementById('popupContainer').style.display='none';
+}
